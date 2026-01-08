c5ff6e34bc82bd155ce04235cdca1eee6cef3466
Vojta Soukup <vojta.soukup76@gmail.com>
2026-01-08 18:30:22 +0100

    vlocky a test9

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function vlocka() {
     var vlocka = document.getElementsByClassName("vlocka")[0]
-    for (let index = 0; index < 10; index++) {
-        const novaVlocka = vlocka.cloneNode();
-document.body.appendChild(novaVlocka)
-       novaVlocka.style.left = Math.random()* 100 + "vw"
-       novaVlocka.style.animationDelay = Math.random()* 10 + "s"
-
+var mnesic=new Date().getMonth();
+    if (mnesic<1 || mnesic==11) {
         
+        
+        for (let index = 0; index < 10; index++) {
+            const novaVlocka = vlocka.cloneNode();
+            document.body.appendChild(novaVlocka)
+            novaVlocka.style.left = Math.random()* 100 + "vw"
+            novaVlocka.style.animationDelay = Math.random()* 10 + "s"
+            
+            
+        }
+        
+    }else{
+        vlocka.remove()
     }
-    
 }
 vlocka()
